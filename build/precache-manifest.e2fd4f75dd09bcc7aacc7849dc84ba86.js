2a893af12aceab40713ba67a9d8ba6fa20056812
zhq <zhq199622@163.com>
2020-02-13 11:09:24 +0800

    第一次 build

---
+++ build/precache-manifest.e2fd4f75dd09bcc7aacc7849dc84ba86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a74574a24374585880d9858fe15817b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a423bdadd733343298e",
+    "url": "/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "9810feb023747030e964",
+    "url": "/static/js/2.fa2d0860.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.fa2d0860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a423bdadd733343298e",
+    "url": "/static/js/main.8dcd560d.chunk.js"
+  },
+  {
+    "revision": "c56ad2ce85d82c6737f6",
+    "url": "/static/js/runtime-main.f2239a7c.js"
+  }
+]);
